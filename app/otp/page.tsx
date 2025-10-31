a3b6971227484d459801fb7176770e564d4ed658
Caio <caioh.maiab@gmail.com>
2025-10-30 21:33:23 -0300

    ajustando paginas

--- app/otp/page.tsx
+++ app/otp/page.tsx
@@ -1,9 +1,10 @@
 "use client"
 
+import { Suspense } from "react"
 import { useSearchParams } from "next/navigation"
 import { OTPForm } from "@/components/otp-form"
 
-export default function OTPPage() {
+function OTPContent() {
   const searchParams = useSearchParams()
   const email = searchParams.get("email")
 
@@ -15,11 +16,17 @@ export default function OTPPage() {
     return null
   }
 
+  return <OTPForm email={email} />
+}
+
+export default function OTPPage() {
   return (
     <div className="flex min-h-svh w-full">
       <div className="flex w-full items-center justify-center p-6 lg:w-1/2">
         <div className="w-full max-w-xs">
-          <OTPForm email={email} />
+          <Suspense fallback={<div>Carregando...</div>}>
+            <OTPContent />
+          </Suspense>
         </div>
       </div>
       <div className="relative hidden w-1/2 lg:block">
